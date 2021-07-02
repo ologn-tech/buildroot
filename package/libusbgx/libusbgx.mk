387f6c9775793fa28bfcfef29f06e51240679236
Huy Duong <huy.duong@employmenthero.com>
2021-07-03 00:09:18 +0700

    Add link to libusbgx-0.2.1
    Signed-off-by: Huy Duong <huy.duong@employmenthero.com>

--- package/libusbgx/libusbgx.mk
+++ package/libusbgx/libusbgx.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-LIBUSBGX_VERSION = 0.2.0
-LIBUSBGX_SITE = /home/qhuyduong/workspace/android/libusbgx
-LIBUSBGX_SITE_METHOD = local
+LIBUSBGX_VERSION = 0.2.1
+LIBUSBGX_SITE = $(call github,Car-Retrofit,libusbgx,libusbgx-v$(LIBUSBGX_VERSION))
 LIBUSBGX_LICENSE = GPL-2.0+ (examples), LGPL-2.1+ (library)
 LIBUSBGX_LICENSE_FILES = COPYING COPYING.LGPL
 LIBUSBGX_DEPENDENCIES = host-pkgconf libconfig
